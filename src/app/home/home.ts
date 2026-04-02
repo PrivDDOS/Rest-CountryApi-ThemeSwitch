1f04ce707834b266e5dc07057ea5a17f4c1dea44
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-04-02 10:56:30 +0700

    added search function for the input and filter for the filter by region

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component , OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  data: any[] = []
+  filteredData: any[] = []
+
+  constructor(private http: HttpClient) {}
   
+  ngOnInit(): void {
+    this.http.get<any[]>('/assets/data.json')
+      .subscribe(res => {
+        this.data = res;
+        this.filteredData = res;
+      });
+  }
+
+  searchCountry(country: string) {
+    const query = country.trim().toLowerCase();
+
+    if (!query) {
+      this.filteredData = [...this.data];
+      return;
+    }
+
+    this.filteredData = this.data.filter(countryItem =>
+      countryItem.name.toLowerCase().includes(query)
+    );
+  }
+
+  getRegion(region: string) {
+    this.filteredData = this.data.filter(country => country.region === region);
+  }
+
 }
